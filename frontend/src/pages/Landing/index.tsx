e17a738e554c635b6ac1f123d30b521b7b6bf040
Jaime Barbosa <jaimebs@gmail.com>
2020-08-07 16:31:03 -0300

    refactor: TeacherForm and TeacherList

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
@@ -10,8 +10,21 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import "./styles.css";
 
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function Landing() {
+  const [numberConnectios, setNumberConnections] = useState(0);
+
+  async function numbersOfConnections() {
+    const connections = await api.get("connections");
+    const { total } = connections.data;
+    setNumberConnections(total);
+  }
+
+  useEffect(() => {
+    numbersOfConnections();
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -39,7 +52,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas{" "}
+          Total de {numberConnectios} conexões já realizadas{" "}
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
